abf1c4aeffb9f6bbc249c419b93fb53467c283e6
Mark Hoath <markahoath@gmail.com>
2018-12-01 17:38:09 +1100

    Initial Commit

---
+++ tigerspike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tigerspike
+//
+//  Created by Mark Hoath on 1/12/18.
+//  Copyright © 2018 Swift Almanac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
